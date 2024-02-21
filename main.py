365e20643031071ae17c22b204479e9b8ac106e0
Raphaël <126982829+raphrapide100@users.noreply.github.com>
2024-02-21 19:48:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import random
 import string
 
 app = Flask(__name__)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
 
 # Tes fonctions existantes
 def generate_random_code(length=18):
